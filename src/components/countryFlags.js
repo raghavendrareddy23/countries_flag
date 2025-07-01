796e33d3d9d16e37f4373a3393f688e5f3101db3
raghavendrareddy23 <70099426+raghavendrareddy23@users.noreply.github.com>
2025-07-01 22:30:49 +0530

    added all files

---
+++ src/components/countryFlags.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import '../index.css'
+
+function CountryFlags() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://xcountries-backend.azurewebsites.net/all"
+        );
+        // console.log(response.data)
+        if (response.status === 200) {
+          const resData = response.data;
+          setData(resData);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchData()
+  }, []);
+
+//   console.log(data)
+  return <div className="grid-container">
+    {
+        data?.map((index)=>(
+           <div key={index} className="card">
+            <img src={index.flag} alt={index.name}/>
+            <p>{index.name}</p>
+           </div> 
+        ))
+    }
+  </div>;
+}
+
+export default CountryFlags;
